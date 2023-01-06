ee6386e3b2d4fdf169cf41f0bb6f0567d7d4b0e1
Lewis Knapp <lknapp1888@gmail.com>
2023-01-06 23:29:28 +0000

    add todo item constructor and todolist main storage object

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
 import './style.css';
+import Todo, {todoList} from './todo-list.js';
+
+const first = new Todo('first test title', 'dummy desc one', 'Sunday', 1, 'Project Zeus');
+const second = new Todo('Second title', 'dummy desc two', 'Monday', 2, 'Brexit');
+const third = new Todo('third title', 'dummy desc three', 'Tuesday', 3, 'Get new job');
+
+
+console.log(todoList.list)
---
+++ src/todo-list.js
@@ -0,0 +1,35 @@
+export default function Todo (title, description, dueDate, priority, project) {
+    this.title = title;
+    this.description = description;
+    this.dueDate = dueDate;
+    this.priority = priority;
+    this.project = project;
+    this.referenceNum = todoList.itemTicker;
+    todoList.addToList(this);
+    }
+
+Todo.prototype.getDescription = function() { console.log(`The description is ${this.description}`)};
+Todo.prototype.getTitle = function () { console.log(`The title is ${this.title}`);}
+Todo.prototype.getDueDate = function () { console.log(`The due date is ${this.dueDate}`);}
+Todo.prototype.getPriority = function () { console.log(`The priority level is ${this.priority}`);}
+Todo.prototype.getProject = function () { console.log(`The project is ${this.project}`);}
+
+
+export const todoList = {
+    list: [],
+    /* itemTicker acts as reference number for any todo item added. when the todo constructer is called
+     the value is assigned as reference number, then increment by 1*/
+    itemTicker: 1,
+    addToList: function(item) {
+        this.list.push(item);
+        this.itemTicker++;
+    },
+    removeFromList: function(itemRefNumber) {
+        for (let i = 0; i < this.list.length; i++) {
+           if (this.list[i].referenceNum === itemRefNumber) {
+            this.list.splice(i, 1);
+           }
+        }
+        
+    }
+}
