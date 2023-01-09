c00e9d7104545ab714da2d81abf5c2ff7979c61a
Lewis Knapp <lknapp1888@gmail.com>
2023-01-09 23:41:34 +0000

    add edit item UI and edit btn event listener

--- src/UI.js
+++ src/UI.js
@@ -4,13 +4,15 @@ import { todoListFilters } from './filter.js';
 export const uiLoad = {
     loadPage: function (itemArray) {
         //itemArray is the default list to filter by, can be set in index.js
-        this.initBtns();
         this.loadItemArray(itemArray);
+        this.initFilterBtns();
+        this.initEditButton(itemArray); 
     },
 
     loadItem: function (item) {
         const itemDiv = document.createElement('div');
         itemDiv.classList.add('todoItem');
+        itemDiv.classList.add(`todoItem-${item.referenceNum}`);
         itemDiv.innerHTML = `
         <p>${item.title}</p>
         <p>${item.description}</p>
@@ -21,7 +23,7 @@ export const uiLoad = {
                 <p>${item.priority}</p>
                 <p>${item.project}</p>
             </div>
-            <button class="itemEditBtn" data-editBtn-ref="${item.referenceNum}">Edit</button>
+            <button class="itemEditBtn editBtn${item.referenceNum}">Edit</button>
         </div>
         `;
         uiStorage.main.appendChild(itemDiv);
@@ -51,20 +53,68 @@ export const uiLoad = {
                 uiStorage.main.appendChild(dateTitleTwo);
             }
         }
+        this.initEditButton(itemArray); 
     },
 
-    initBtns: function() {
+    editItem: function (refNum) {
+        const todoItem = document.querySelector(`.todoItem-${refNum}`);
+        while (todoItem.lastChild) {
+            todoItem.removeChild(todoItem.lastChild);
+        }
+        todoItem.innerHTML = `
+        <input type="text" name="" id="" placeholder="title">
+        <textarea name="" id="" cols="30" rows="6"></textarea>
+        <div class="itemBottom">
+            <div class="itemInfo">
+                <input id="testDate" type="date">
+                <label for="priorityLevel">Priority:</label>
+                <select id="priorityLevel" name="priorityLevel">
+                  <option value=1>1</option>
+                  <option value=2>2</option>
+                  <option value=3>3</option>
+                  <option value=4>4</option>
+                </select>
+                <label for="UIProjects">Project:</label>
+                <input type="text" list="UIProjects" />
+                    <datalist id="UIProjects">
+                    <option>Proj</option>
+                    <option>project</option>
+                    <option>project 2</option>
+                    <option>fake project</option>
+                    </datalist>
+            </div>
+            <button class="itemSubmitBtn submitBtn${refNum}">Submit</button>
+        </div>
+        `
+    },
+
+    submitItem: function (refNum) {
+
+    },
+
+    initFilterBtns: function() {
+       
+       //filters
         uiStorage.todayFilterBtn.addEventListener('click', () => {
                 uiLoad.loadItemArray(todoListFilters.filterToday(todoList.list))
         });
-
         uiStorage.sevenDayFilterBtn.addEventListener('click', () => {
                 uiLoad.loadItemArray(todoListFilters.filterSevenDay(todoList.list))
         });
         uiStorage.allBtn.addEventListener('click', () => {
-            uiLoad.loadItemArray(todoListFilters.filterAll(todoList.list))
-        });
+            uiLoad.loadItemArray(todoListFilters.filterAll(todoList.list));
 
+        });
+    },
+
+    initEditButton: function (item) {
+        for (let i = 0; i < item.length; i++) {
+            const editBtn = document.querySelector(`.editBtn${item[i].referenceNum}`);
+            editBtn.addEventListener('click', () => {
+                console.log(`hello ${item[i].referenceNum}`);
+                this.editItem(item[i].referenceNum);
+            });
+        }
     }
 }
 
--- src/index.js
+++ src/index.js
@@ -4,17 +4,14 @@ import { todoListFilters } from './filter.js';
 import {uiLoad, uiStorage} from './UI';
 import Project from './projects';
 
-const first = new Todo('first test title', 'dummy desc one', '2023-01-08', 1, 'Project Zeus');
-const second = new Todo('Second title', 'dummy desc two', '2023-01-10', 2, 'Brexit');
-const third = new Todo('third title', 'dummy desc three', '2023-01-28', 1, 'Brexit');
+const first = new Todo('first test title', 'dummy desc one', '2023-01-09', 1, 'Project Zeus');
+const second = new Todo('Second title', 'dummy desc two', '2023-01-28', 2, 'Brexit');
+const third = new Todo('third title', 'dummy desc three', '2023-01-10', 1, 'Brexit');
 
 const brexit = new Project('Brexit', 'This is a project for Britain to leave the EU');
 
 // set the filter function to dictate the default starting filter
 uiLoad.loadPage(todoListFilters.filterToday(todoList.list)) 
-
-console.log(todoList.list);
-console.log(todoListFilters.sortDate(todoList.list))
 
 
 
